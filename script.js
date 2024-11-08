5dbeb0904d58460062c14ae7e2042da70710a781
NikithaGiri1213 <nikithagiri1213@gmail.com>
2024-11-08 18:01:22 +0530

    changes commit
    changes commit

--- script.js
+++ script.js
@@ -2,74 +2,59 @@ const todoForm = document.querySelector('form');
 const todoInput = document.querySelector('#todo-input');
 const todoListUL = document.querySelector('#todo-list');
 
-let allTodos = getTodos();
-updateTodoList();
+// Load saved todos from localStorage
+let todos = JSON.parse(localStorage.getItem('todos') || '[]');
 
-todoForm.addEventListener('submit', function(event) {
+// Update the todo list UI
+function updateTodoList() {
+    todoListUL.innerHTML = '';
+    todos.forEach((todo, index) => {
+        const todoItem = document.createElement('li');
+        todoItem.className = 'todo';
+        todoItem.innerHTML = `
+            <input type="checkbox" id="todo-${index}" ${todo.completed ? 'checked' : ''}>
+            <label for="todo-${index}" class="custom-checkbox">
+                <img src="./icons/check_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png" alt="check" height="25px" width="25px">
+            </label>
+            <label for="todo-${index}" class="todo-text">${todo.text}</label>
+            <button class="delete-button">
+                <img src="./icons/delete_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png" alt="delete" height="25px" width="25px">
+            </button>
+        `;
+        
+        // Delete todo
+        todoItem.querySelector('.delete-button').addEventListener('click', () => {
+            todos.splice(index, 1);
+            saveTodos();
+            updateTodoList();
+        });
+        
+        // Toggle todo completion
+        todoItem.querySelector('input').addEventListener('change', (e) => {
+            todos[index].completed = e.target.checked;
+            saveTodos();
+        });
+        
+        todoListUL.append(todoItem);
+    });
+}
+
+// Save todos to localStorage
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// Handle form submission (add new todo)
+todoForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    addTodo();
-})
-
-function addTodo(){
-    const todoText = todoInput.value.trim();
-    if(todoText.length > 0){
-        const todoObject = {
-            text: todoText,
-            completed: false
-        }
-        allTodos.push(todoObject);
-        updateTodoList();
+    const text = todoInput.value.trim();
+    if (text) {
+        todos.push({ text, completed: false });
         saveTodos();
+        updateTodoList();
         todoInput.value = '';
     }
-}
-function updateTodoList(){
-    todoListUL.innerHTML = '';
-    allTodos.forEach((todo, todoIndex) => {
-        todoItem = createTodoItem(todo, todoIndex);
-        todoListUL.append(todoItem)
-    })
-}
-function createTodoItem (todo, todoIndex){
-    const todoId = 'todo-'+todoIndex;
-    const todoLI = document.createElement('li');
-    const todoText = todo.text;
-    todoLI.className = 'todo';
-    todoLI.innerHTML = `
-        <input type="checkbox" id="${todoId}">
-        <label for="${todoId}" class="custom-checkbox">
-            <img fill="transparent" src="./icons/check_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png" alt="checkIcon" height="25px" width="25px">
-        </label>
-        <label for="${todoId}" class="todo-text">
-            ${todoText}
-        </label>
-        <button class="delete-button">
-            <img fill="var(--secondary-color)" 
-            src="./icons/delete_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png" alt="deleteIcon" height="25px" width="25px">
-        </button>
-    `
-    const deleteButton = todoLI.querySelector('.delete-button');
-    deleteButton.addEventListener('click', ()=>{
-        deleteTodoItem(todoIndex)
-    })
-    const checkbox = todoLI.querySelector('input');
-    checkbox.addEventListener("change", ()=>{
-        allTodos[todoIndex].completed = checkbox.checked;
-        saveTodos();
-    })
-    checkbox.checked = todo.completed;
-    return todoLI;
-}
-function deleteTodoItem(todoIndex){
-    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
-    saveTodos();
-    updateTodoList();
-}
-function saveTodos(){
-    const todoJson = JSON.stringify(allTodos);
-    localStorage.setItem('todos', todoJson);
-}
-function getTodos(){
-    const todos = localStorage.getItem('todos') || '[]';
-    return JSON.parse(todos)
-}
+});
+
+// Initial call to display the todo list
+updateTodoList();
